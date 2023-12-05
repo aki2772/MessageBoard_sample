d7f7d40e94850289f54e1426d639eedf5c02aa04
aki2772 <anbeiqingzi@gmail.com>
2023-12-05 11:18:19 +0900

    メッセージの保管関数を更新(infraPackage)

--- Go/infra/message_repository.go
+++ Go/infra/message_repository.go
@@ -7,6 +7,8 @@ package infra
 
 import (
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/aki2772/MessageBoard_sample/Go/repository" // 独自パッケージ
 )
@@ -21,11 +23,28 @@ type MessageRepository struct {
 // / </summary>
 // / <param name="message">メッセージ</param>
 // / <returns>エラー</returns>
-func (mr MessageRepository) Save(message string) string {
-	// ファイルにメッセージを書き込む
-	fmt.Println("Save")
-	fmt.Println("message: " + message)
-	return ""
+func (mr MessageRepository) Save(message string) error {
+	// 書き込み先のファイルを開く
+	f, err := os.OpenFile(mr.FilePath, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0666)
+	// 存在しないならエラー
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+	defer f.Close()
+
+	// メッセージをバイト文字列に変換(改行込み)
+	d := []byte(message + "\n")
+
+	// 書き込み
+	n, err := f.Write(d)
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+
+	fmt.Printf("%d bytesの書き込み", n)
+	return nil
 }
 
 // / <summary>
