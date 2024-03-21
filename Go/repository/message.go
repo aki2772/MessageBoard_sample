d7dc8c3379a289e52f432d9e6f486cc8799c98e7
aki2772 <anbeiqingzi@gmail.com>
2024-03-21 12:06:59 +0900

    ここにDB情報は含めない

--- Go/repository/message.go
+++ Go/repository/message.go
@@ -5,18 +5,12 @@
 
 package repository
 
-import (
-	"github.com/aki2772/MessageBoard_sample/Go/model" // 独自パッケージ
-
-	"database/sql"
-
-	_ "github.com/go-sql-driver/mysql"
-)
+import "github.com/aki2772/MessageBoard_sample/model"
 
 type MessageRepository interface {
 	/*Save(*model.Message) error       // メッセージを保管する
 	List() ([]*model.Message, error) // 保管されているメッセージのリストを引き出す*/
 
-	DBSave(*model.Message, *sql.DB) error     // メッセージを保管する
-	DBList(*sql.DB) ([]*model.Message, error) // 保管されているメッセージのリストを引き出す
+	DBSave(*model.Message) error       // メッセージを保管する
+	DBList() ([]*model.Message, error) // 保管されているメッセージのリストを引き出す
 }
